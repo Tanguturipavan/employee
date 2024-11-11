837bf07e4316262d965f6d845a2e3f719b52bb80
Tanguturipavan <142770142+Tanguturipavan@users.noreply.github.com>
2024-11-11 20:29:50 +0530

    Add files via upload

---
+++ employee_manage.sql
@@ -0,0 +1,6 @@
+use employee;
+CREATE TABLE empdata ( Id INT PRIMARY KEY, Name VARCHAR(255), Email_Id VARCHAR(255), Phone_no VARCHAR(15), Address VARCHAR(255), Post VARCHAR(255), SALARY DECIMAL(10,2));
+select * from empdata;
+
+
+
